a1c3aa6c3446f9369194c7c610879c9502e834a7
Matthew Stobo <>
2018-08-23 21:11:06 +1000

    Respond with literal 1:1 query to city name. TODO: Add fuzzy / predictive search.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,9 +1,23 @@
 // routes.js
 
-module.exports = function(app, db) {
-    app.get('/cities', (request, response) => {
-        //Logic goes here
-        response.send("Response being output")
-    });
-}
+let cityData = require("../data/city.list.json");
 
+module.exports = function(app, db) {
+  app.get("/cities/:query", (request, response) => {
+
+    const queryString = request.params.query;
+    const details = { 'query': request.params.query };
+
+    console.log('​module.exports -> details', details);
+    console.log('​module.exports -> queryString', queryString);
+    let currCity = "";
+
+    cityData.forEach(function(city) {
+      if (city.name == details.query) {
+        currCity = city;
+      }
+    });
+
+    response.json(currCity);
+  });
+};
